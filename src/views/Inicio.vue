7304654a8334c5877eada7b412a8064c579da8b5
luisEfe80 <luismendez80@hotmail.com>
2021-10-06 14:52:49 -0400

    Update Inicio.vue

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -20,10 +20,10 @@ section
                 p.banner-principal__datos__item__texto 228124
               li.mb-3.banner-principal__datos__item
                 p.h6.mb-0.me-3.banner-principal__datos__item__titulo Total Horas: 
-                p.banner-principal__datos__item__texto 3984 Horas
+                p.banner-principal__datos__item__texto 3984
               li.mb-3.banner-principal__datos__item
                 p.h6.mb-0.me-3.banner-principal__datos__item__titulo Duración en meses: 
-                p.banner-principal__datos__item__texto 27 meses
+                p.banner-principal__datos__item__texto 27
               li.mb-3.banner-principal__datos__item
                 p.h6.mb-0.me-3.banner-principal__datos__item__titulo Modalidad: 
                 p.banner-principal__datos__item__texto virtual
